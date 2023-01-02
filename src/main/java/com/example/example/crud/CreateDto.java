4c0a8a93a98d82222ea2f0a547bf24b01d928a5b
kwang <hea0408never@naver.com>
2023-01-02 23:52:38 +0900

    CreateDto에 NotBlank 추가

--- src/main/java/com/example/example/crud/CreateDto.java
+++ src/main/java/com/example/example/crud/CreateDto.java
@@ -3,11 +3,16 @@ package com.example.example.crud;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class CreateDto {
 
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
+    @NotBlank
     private String nickname;
 }
